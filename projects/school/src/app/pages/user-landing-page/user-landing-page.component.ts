3f50881ad796c9239cf2ad8bcb48fcb0abf8cb3a
Weaponz <weaponz326@gmail.com>
2022-03-05 17:50:25 +0000

    .

--- projects/school/src/app/pages/user-landing-page/user-landing-page.component.ts
+++ projects/school/src/app/pages/user-landing-page/user-landing-page.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component'
+import { UserTopComponent } from 'projects/personal/src/app/components/suite-landing/user-top/user-top.component';
+
 import { AdminApiService } from '../../services/modules/admin-api/admin-api.service';
+import { SettingsApiService } from '../../services/modules/settings-api/settings-api.service';
 
 
 @Component({
@@ -13,11 +17,16 @@ export class UserLandingPageComponent implements OnInit {
 
   constructor(
     private router: Router,
-    // private adminApi: AdminApiService
+    private adminApi: AdminApiService,
+    private settingsApi: SettingsApiService
   ) { }
+
+  @ViewChild('userTopComponentReference', { read: UserTopComponent, static: false }) userTop!: UserTopComponent;
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
 
   accountsData: any;
   isAccountLoading: boolean = false;
+  isAccountChecking: boolean = false;
 
   ngOnInit(): void {
     this.getUserAccountUser();
@@ -26,25 +35,55 @@ export class UserLandingPageComponent implements OnInit {
   getUserAccountUser(){
     this.isAccountLoading = true;
 
-    // this.adminApi.getUserAccountUser()
-    //   .then(
-    //     (res: any) => {
-    //       console.log(res);
-    //       this.accountsData = res.docs;
-    //       this.isAccountLoading = false;
-    //     },
-    //     (err: any) => {
-    //       console.log(err);
-    //       this.isAccountLoading = false;
-    //     }
-    //   )
+    this.adminApi.getUserAccountUser()
+      .then(
+        (res: any) => {
+          console.log(res);
+          this.accountsData = res.docs;
+          this.isAccountLoading = false;
+        },
+        (err: any) => {
+          console.log(err);
+          this.isAccountLoading = false;
+          this.connectionToast.openToast();
+        }
+      )
   }
+
+  checkAccountActive(data: any){
+    this.isAccountChecking = true;
+    console.log(data.data());
+
+    localStorage.setItem('school_id', data.data().account.id);
+
+    this.settingsApi.getSubscription()
+      .then(
+        (res: any) => {
+          console.log(res.data());
+          this.isAccountChecking = false;
+
+          if (res.data().status == "Active" || res.data().status == "Active" && data.data().created_by == localStorage.getItem('personal_id')){
+            this.router.navigateByUrl('/home');
+          }
+          else {
+            localStorage.removeItem('school_id');
+            this.userTop.openSubscriptionModal();
+          }
+        },
+        (err: any) => {
+          console.log(err);
+          this.isAccountChecking = false;
+          this.connectionToast.openToast();
+        }
+      )
+  }
+
 
   activateAccount(data: any){
     console.log(data);
-    localStorage.setItem('restaurant_id', data.data().account.id);
-    localStorage.setItem('restaurant_name', data.data().account.data.name);
-    sessionStorage.setItem('restaurant_account_user_id', data.id);
+    localStorage.setItem('school_id', data.data().account.id);
+    localStorage.setItem('school_name', data.data().account.data.name);
+    sessionStorage.setItem('school_account_user_id', data.id);
     this.router.navigateByUrl('/home');
 
     // TODO: store selected account in server session
