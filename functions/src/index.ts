65b19222227608c9014239225049a383513be573
Weaponz <weaponz326@gmail.com>
2022-03-14 07:36:48 +0000

    fixed cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,18 +11,18 @@ admin.initializeApp();
 //   response.send("Hello from Firebase!");
 // });
 
-export const paystackSubscriptionWebhook = async (data: any) => {
-  const accountId = data.customer.metadata.account;
+export const paystackSubscriptionWebhook = functions.https.onRequest((req, res) => {
+  const accountId = req.body.customer.metadata.account;
   const subscriptionData = {
-    subscription_type: data.customer.metadata.subscription_type,
-    billing_frequency: data.customer.metadata.billing_frequency,
-    number_users: data.customer.metadata.number_users,
-    email: data.customer.email,
+    subscription_type: req.body.customer.metadata.subscription_type,
+    billing_frequency: req.body.customer.metadata.billing_frequency,
+    number_users: req.body.customer.metadata.number_users,
+    email: req.body.customer.email,
     status: "Active",
   };
 
-  const subscriptionRef = functions.firestore.collection.(
-      "restaurant/module_settings/restaurant_subscription");
+  const subscriptionRef = admin.firestore().collection("restaurant/module_settings/restaurant_subscription");
+  subscriptionRef.doc(accountId).set(subscriptionData);
 
-  await subscriptionRef.doc(accountId).set(subscriptionData);
-};
+  res.sendStatus(200);
+});
